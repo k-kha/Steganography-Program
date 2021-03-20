a316520de8a6f9199cf6f605c8887b33e983a4e2
k-kha <48115819+k-kha@users.noreply.github.com>
2021-03-20 00:59:51 -0400

    Update steganography.py

--- steganography.py
+++ steganography.py
@@ -6,15 +6,12 @@ from PIL import Image
 import numpy as np
 import sys
 
-#Encryption
-import base64
-from Crypto.Cipher import AES
-from Crypto import Random
-from Crypto.Protocol.KDF import PBKDF2
-
 from algorithm import isPrime
 from algorithm import nthPrime
 from algorithm import getMod
+from encryption import get_private_key
+from encryption import encrypt
+from encryption import decrypt
 from decode import decToBin
 from decode import get2LSB
 from decode import filter2LSB
@@ -168,33 +165,6 @@ def finalImage(data, new_pix, bytes_needed):
     final = final.reshape(data.shape)
     return final
 # 1111111111 Encoding end 1111111111
-    
-# 4444444444 Encryption 4444444444
-def get_private_key(pw):
-    salt = b"CIS628 cryptography"
-    temp = PBKDF2(pw, salt, 64, 1000)
-    pr_key = temp[:32]
-    return pr_key
-
-def encrypt(raw, pw):   
-    blockSize = 16
-    pad = lambda x: x + (blockSize - len(x) % blockSize) * chr(blockSize - len(x) % blockSize)
-    pr_key = get_private_key(pw)
-    padded = pad(raw)
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(pr_key, AES.MODE_CBC, iv)
-    encryptedMsg = base64.b64encode(iv + cipher.encrypt(padded))
-    return encryptedMsg
-
-def decrypt(encMsg, pw):
-    unpad = lambda x: x[:-ord(x[len(x) - 1:])]
-    pr_key = get_private_key(pw)
-    encMsg = base64.b64decode(encMsg)
-    iv = encMsg[:16]
-    cipher = AES.new(pr_key, AES.MODE_CBC, iv)
-    decryptedMsg = unpad(cipher.decrypt(encMsg[16:]))
-    return decryptedMsg
-# 4444444444 Encryption end 4444444444
           
 def encoding(data, listdict, bytes_needed, pixels_bin, msg):             
     a = bytesNeededBin(bytes_needed, pixels_bin) #picture in 8bit binary: ['11111111', ...]
